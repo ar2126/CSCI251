21b1609590684081ae9cb432cc1e532c2db715ac
ar2126 <ar2126@cs.rit.edu>
2018-10-02 22:43:00 -0400

    Reorganized files

--- Whistleblower/src/Leaker.java
+++ Whistleblower/src/Leaker.java
@@ -1,98 +1,74 @@
-import java.io.*;
+
+import java.io.IOException;
 import java.lang.NumberFormatException;
 import java.math.BigInteger;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 
 
 /**
- * Class Leaker contains the main method for the 'Leaker' portion of the Whilstleblower project. It is used for
- * parsing the input from the command line, encoding the message to plaintext, calling Encrypt to convert the plaintext
- * to ciphertext, and converting the ciphertext into the payload for the datagram
+ * Class Leaker contains the main method for the 'Leaker' portion of the Whilstleblower project. It parses the input
+ * from the command line, encodes the message to plaintext, calls RSA to read files &
+ * convert the plaintext to ciphertext, and calls upon the ReporterProxy to create & send a payload
  *
  * @author	Aidan Rubenstein
- * @version 24-Apr-2018
+ * @version 01-May-2018
  */
+
 public class Leaker {
 
-    public static String rhost;
-    public static String lhost;
-    public static int rport = 0;
-    public static int lport = 0;
-    public static String publicfile;
-    public static String message;
-    public static BigInteger exponent;
-    public static BigInteger modulus;
+    private static String rhost;
+    private static int rport = 0;
+    private static String lhost;
+    private static int lport = 0;
+    private static String file;
+    private static String message;
 
     /**
      * Main method for Leaker.It parses the input from the command line, encodes the message to plaintext, calls
-     * Encrypt to convert the plaintext to ciphertext, and converts the ciphertext into the payload for the datagram
+     * RSA to convert the plaintext to ciphertext, and calls upon the ReporterProxy to create & send a payload
      *
      * @param args The specified program arguments
      *
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
-        //Checks argument lengths and parses all arguments as intended
-        if (args.length != 6) {
-            System.err.print("ERROR: Insufficient number of arguments");
+        if(args.length != 6) {
+            System.err.println("Usage: java Leaker <rhost> <rport> <lhost> <lport> <publickeyfile> <message>");
             System.exit(1);
         }
+
+        rhost = args[0];
+
         try {
-
-            rhost = args[0];
-
             rport = Integer.parseInt(args[1]);
-
-            lhost = args[2];
-
-            lport = Integer.parseInt(args[3]);
-
-            publicfile = args[4];
-
-        } catch (NumberFormatException e){
-            System.err.print("ERROR: one or more arguments cannot be parsed");
+        } catch(NumberFormatException e) {
+            System.err.print("Error: Cannot parse " + args[1]);
             System.exit(1);
         }
 
-        // Extracts the exponent and modulus from the 2 lines in the public key file as 2048-bit integers
+        lhost = args[2];
+
         try {
-            BufferedReader reader = new BufferedReader(
-                    new FileReader(publicfile));
-
-            exponent = new BigInteger(reader.readLine());
-            modulus = new BigInteger(reader.readLine());
-
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.err.print("ERROR: Public key file " + publicfile + " does not exist\n");
+            lport = Integer.parseInt(args[3]);
+        } catch(NumberFormatException e) {
+            System.err.print("Error: Cannot parse " + args[3]);
             System.exit(1);
         }
+
+        file = args[4];
+
+
         message = args[5];
 
-        //Converts the plaintext to ciphertext using Encrypt
-        Encrypt e = new Encrypt(exponent, modulus);
-        byte[] ciphertext = e.encrypt(message);
+        RSA rsa = new RSA();
+        rsa.readPub(file);
+        byte[] ciphertext = rsa.encrypt(message);
 
-        DatagramSocket mailbox = new DatagramSocket(
-                new InetSocketAddress(lhost, lport));
+        ReporterProxy proxy = new ReporterProxy(
+                new DatagramSocket(new InetSocketAddress(lhost, lport)),
+                new InetSocketAddress(rhost, rport));
 
-        SocketAddress rMailbox = new InetSocketAddress(rhost, rport);
-
-        ByteArrayOutputStream outByte = new ByteArrayOutputStream();
-        DataOutputStream out = new DataOutputStream(outByte);
-
-        //Writes the ciphertext containing the length of the ciphertext and the ciphertext itself and sends it to
-        //the payload as a byte array
-        out.writeShort(ciphertext.length);
-        out.write(ciphertext, 0, ciphertext.length);
-        out.close();
-
-        //Creates a payload as a byte array and sends the payload to the Reporter
-        byte[] payload = outByte.toByteArray();
-        mailbox.send(new DatagramPacket(payload, payload.length, rMailbox));
+        proxy.leak(new BigInteger(ciphertext));
     }
 }
--- Whistleblower/src/Reporter.java
+++ Whistleblower/src/Reporter.java
@@ -1,104 +1,59 @@
-import java.io.*;
+
+import java.io.IOException;
 import java.lang.NumberFormatException;
-import java.math.BigInteger;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 
 
 /**
  * Class Reporter contains the main method for the 'Reporter' portion of the Whilstleblower project. It is used for
- * converting the datagram's payload to the ciphertext, decrypting the ciphertext using RSA, yielding the plaintext,
- * and decoding the plaintext, yielding the message string.
+ * converting the datagram's payload to the ciphertext, reading the private file & decrypting the ciphertext using RSA,
+ * yielding the plaintext, and calling on the ListenerProxy to receive messages and report them
  *
  * @author	Aidan Rubenstein
- * @version 24-Apr-2018
+ * @version 01-May-2018
  */
+
 public class Reporter {
-    public static String rhost;
-    public static int rport = 0;
-    public static String privatefile;
-    public static BigInteger pExp;
-    public static BigInteger modulus;
-    public static Decrypt d;
+    private static String rhost;
+    private static int rport = 0;
+    private static String file = "";
 
     /**
-     * Main method for Reporter.It converts the datagram's payload to the ciphertext, decrypts the ciphertext using RSA,
-     * yielding the plaintext, and decodes the plaintext, yielding the message string.
+     * Main method for Reporter. It converts the datagram's payload to the ciphertext, reads the private file &
+     * decrypts the ciphertext using RSA, yielding the plaintext, and calls on the ListenerProxy to
+     * receive messages and report them
      *
      * @param args The specified program arguments
      *
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
-        //Checks argument lengths and parses all arguments as intended
-        if (args.length != 3) {
-            System.err.print("ERROR: Insufficient number of arguments");
+        if(args.length != 3) {
+            System.err.print("Usage: java Reporter <rhost> <rport> <privatefile>");
             System.exit(1);
         }
-        try {
-            rhost = args[0];
 
+        rhost = args[0];
+
+        try {
             rport = Integer.parseInt(args[1]);
-
-            privatefile = args[2];
-        }catch(NumberFormatException e){
-            System.err.print("ERROR: one or more arguments cannot be parsed");
+        } catch(NumberFormatException e) {
+            System.err.print("Error: Cannot parse " + args[1]);
             System.exit(1);
         }
+        file = args[2];
 
-        // Extracts the exponent and modulus from the 2 lines in the private key file as 2048-bit integers
-        try {
-            BufferedReader reader = new BufferedReader(
-                    new FileReader(privatefile));
+        RSA rsa = new RSA();
+        rsa.readPriv(file);
 
-            pExp = new BigInteger(reader.readLine());
-            modulus = new BigInteger(reader.readLine());
+        ReporterModel model = new ReporterModel(rsa);
+        LeakerProxy proxy = new LeakerProxy(
+                new DatagramSocket(new InetSocketAddress(rhost, rport)));
 
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.err.print("ERROR: Private key file " + privatefile + " does not exist\n");
-            System.exit(1);
-        }
+        proxy.setListener(model);
 
-        //Converts the ciphertext to plaintext using Decrypt
-        d = new Decrypt(pExp, modulus);
-
-        byte[] payload = new byte[260];
-        DatagramPacket pay;
-        DatagramSocket mailbox = new DatagramSocket(
-                new InetSocketAddress(rhost, rport));
-
-        //Opens the Reporter's mailbox to receive any messages from the Leaker to report
-        while(true) {
-            pay = new DatagramPacket(payload, payload.length);
-
-            try {
-                mailbox.receive(pay);
-            } catch(Exception e) {
-
-            }
-
-            try {
-                DataInputStream in = new DataInputStream(new ByteArrayInputStream(payload));
-                short size = in.readShort();
-
-                if(size < 1 || size > 258)
-                    throw new Exception();
-
-                byte[] buf = new byte[size];
-
-                if(size != in.read(buf, 0, size))
-                    throw new Exception();
-                //Prints the final decrypted message to the console
-                System.out.print(d.decrypt(new BigInteger(buf).toByteArray()) + "\n");
-            }
-            //If any errors occurs during the transaction of receiving messages from Leaker, an error message is printed
-            //to standard out
-            catch(Exception e) {
-                System.err.print("ERROR\n");
-            }
-        }
+        while(true)
+            proxy.receive();
     }
 }
